05fd69fb6fd906929d7a1a2c079816f57aea8fd4
Kenyon Tsai <cytsai@gmail.com>
2021-05-10 23:11:43 +0800

    file-backed statedict

--- cosmos_sdk_gym/envs/cosmos_sdk_env.py
+++ cosmos_sdk_gym/envs/cosmos_sdk_env.py
@@ -3,15 +3,31 @@ import subprocess
 import uuid
 import numpy as np
 from collections import defaultdict
+import json
 import gym
 from gym import spaces
 from gym.utils import seeding
 
 
 class statedict(defaultdict):
-    def __missing__(self, key):
-        self[key] = index = len(self)
+    def __init__(self, fn="statedict.json"):
+        self.fn = fn
+        self.update(self.load())
+
+    def __missing__(self, state):
+        print("NEW STATE", state)
+        assert self.load() == self
+        self[state] = index = len(self)
+        with open(self.fn, 'w') as f:
+            json.dump(self, f)
         return index
+
+    def load(self):
+        try:
+            with open(self.fn, 'r') as f:
+                return json.load(f)
+        except:
+            return {}
 
 
 class CosmosSDKEnv(gym.Env):
@@ -43,7 +59,7 @@ class CosmosSDKEnv(gym.Env):
                 reward = (coverage - self._coverage)
                 self._coverage = coverage
             elif line.startswith("ACTION"):
-                assert line.lstrip("ACTION ") == self._action
+                assert line.lstrip("ACTION ") == self._action[:-1]
             elif line.startswith("STATE"):
                 self._range, state = line.lstrip("STATE ").split()
                 self._range, state = int(self._range), self.statedict[state] # + self._range]
@@ -107,11 +123,14 @@ class CosmosSDKEnv(gym.Env):
         return np.array(self.state)
 
     def step(self, action):
-        assert 0.0 <= action < 1.0
-        self._action = str(int(self._range * action) if self._range > 0 else action)
-        # assert action < range
-        self.action_pipe.write(self._action + '\n')
-        self.action_bkup.write(self._action + '\n')
+        if self._range > 0:
+            action = int(self._range * action)
+            assert 0 <= action < self._range
+        else:
+            assert 0.0 <= action < 1.0
+        self._action = str(action) + '\n'
+        self.action_pipe.write(self._action)
+        self.action_bkup.write(self._action)
         self.action_pipe.flush()
         self.state, reward, done = self._parse_output()
         if done:
