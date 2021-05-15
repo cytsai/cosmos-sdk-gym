fbf4a10d672a86439f665ffec430c5f386bcced0
Kenyon Tsai <cytsai@gmail.com>
2021-05-15 23:31:44 +0800

    read queue

--- cosmos_sdk_gym/envs/cosmos_sdk_env.py
+++ cosmos_sdk_gym/envs/cosmos_sdk_env.py
@@ -1,17 +1,19 @@
 import os
 import subprocess
+import threading
+import queue
 import atexit
 import uuid
-import numpy as np
 from collections import defaultdict
 import json
+import numpy as np
 import gym
 from gym import spaces
 from gym.utils import seeding
 
 
-class statedict(defaultdict):
-    def __init__(self, fn="statedict.json", verbose=False):
+class StateDict(defaultdict):
+    def __init__(self, fn="statedict.json", verbose=True):
         self.fn = fn
         self.verbose = verbose
         self.update(self.load())
@@ -19,10 +21,10 @@ class statedict(defaultdict):
     def __missing__(self, state):
         if self.verbose:
             print("NEW STATE", state)
-        assert self.load() == self
+        # assert self.load() == self
         self[state] = index = len(self)
         with open(self.fn, 'w') as f:
-            json.dump(self, f)
+            json.dump(self, f, indent=2)
         return index
 
     def load(self):
@@ -33,11 +35,43 @@ class statedict(defaultdict):
             return {}
 
 
+class ReadQueue():
+    def __init__(self):
+        self.queue = None
+        self.thread = None
+
+    def close(self):
+        if self.queue:
+            del self.queue
+            self.queue = None
+        if self.thread:
+            self.thread.join()
+            self.thread = None
+
+    def reset(self, source):
+        def _readline(source, queue):
+            try:
+                while True:
+                    line = source.readline().decode("utf-8").strip()
+                    queue.put(line)
+            except:
+                pass
+        self.queue = queue.Queue()
+        self.thread = threading.Thread(target=_readline, args=(source, self.queue), daemon=True)
+        self.thread.start()
+
+    def readline(self, timeout=2):
+        try:
+            line = self.queue.get(timeout=timeout)
+        except:
+            line = "TIMEOUT"
+        return line
+
+
 class CosmosSDKEnv(gym.Env):
     metadata = {'render.modes': ['human']}
     dtype = np.float32
     action_max = np.nextafter(dtype(1.0), dtype(0.0))
-
 
     def __init__(self, env_config={}):
         _env_config = {"sdk_path":"/home/cytsai/research/icf/cosmos-sdk", "sdk_config":"-NumBlocks=2 -BlockSize=100", "verbose":False}
@@ -51,16 +85,17 @@ class CosmosSDKEnv(gym.Env):
         self.action_data = None
         self.action_space = spaces.Box(0.0, self.action_max, (1,), self.dtype)
         self.observation_space = spaces.Discrete(128)
-        self.statedict = statedict(verbose=_env_config["verbose"])
+        self.statedict = StateDict()
+        self.readqueue = ReadQueue()
         atexit.register(lambda: self.close())
 
     def _parse_output(self, collect_reward=True):
         state = self.observation_space.n - 1
         reward = 0.0
-        done = False
+        result = ""
         while True:
-            line = self.process.stdout.readline().decode("utf-8").strip()
-            if self.verbose and line.startswith(("COVERAGE", "STATE", "ACTION", "PASS", "FAIL")):
+            line = self.readqueue.readline()
+            if self.verbose and line.startswith(("COVERAGE", "STATE", "ACTION", "PASS", "FAIL", "TIMEOUT")):
                 print(line)
             if line.startswith("COVERAGE") and collect_reward:
                 coverage = float(line.lstrip("COVERAGE "))
@@ -73,19 +108,15 @@ class CosmosSDKEnv(gym.Env):
                 self._range, state = line.lstrip("STATE ").split()
                 self._range, state = int(self._range), self.statedict[state] # + self._range]
                 break
-            elif line.startswith("PASS"):
-                done = True
+            elif line.startswith(("PASS", "FAIL", "TIMEOUT")):
+                result = line.split()[0]
                 break
-            elif line.startswith("FAIL"):
-                done = True
-                reward += 1.0
-                break
-        return state, reward, done
+        return state, reward, result
 
-    def _write_result(self, fail):
-        self.action_data.write(self.sdk_config + '\n')
-        self.action_data.write("FAIL\n" if fail else "PASS\n")
-        self.action_data.write(str(self._coverage) + '\n')
+    def _write_result(self, result):
+        self.action_data.write(' '.join(self.process.args) + '\n')
+        self.action_data.write("COVERAGE " + str(self._coverage) + '\n')
+        self.action_data.write(result + '\n')
 
     def seed(self, seed=None):
         self.action_space.seed(seed)
@@ -101,8 +132,10 @@ class CosmosSDKEnv(gym.Env):
             self.action_data.close()
             self.action_data = None
         if self.process:
+            self.process.stdout.close()
             self.process.terminate()
             self.process = None
+        self.readqueue.close()
 
     def reset(self):
         # 1. close old guide files
@@ -122,9 +155,10 @@ class CosmosSDKEnv(gym.Env):
         self.action_pipe = open(self.action_pipe, "w")
         self.action_data = open(self.action_data, "w")
         # 5. get initial state
+        self.readqueue.reset(self.process.stdout)
         self._coverage = 0.0
-        self.state, _, done = self._parse_output(collect_reward=False)
-        assert not done
+        self.state, _, result = self._parse_output(collect_reward=False)
+        assert not result
         return self.state
 
     def _step(self, line):
@@ -132,12 +166,10 @@ class CosmosSDKEnv(gym.Env):
         self.action_pipe.write(line)
         self.action_data.write(line)
         self.action_pipe.flush()
-        ### self.action_data.flush() ###
-        self.state, reward, done = self._parse_output()
+        self.state, reward, result = self._parse_output()
+        done = len(result) > 0
         if done:
-            self._write_result(reward > 1.0)
-            if reward > 1.0:
-                reward -= 1.0
+            self._write_result(result)
         return self.state, reward, done, {}
 
     def step(self, action):
